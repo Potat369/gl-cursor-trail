56d25da18a9e6c7f8adbdea565b372370a87cb78
Potat369 <yevheniidemian@gmail.com>
2025-09-29 15:16:34 +0300

    Another trail

--- main.c
+++ main.c
@@ -4,6 +4,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <math.h>
+
+const int TRAIL_SIZE = 20;
 
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
 unsigned int createShaderFromFile(GLenum type, char* name);
@@ -36,69 +39,65 @@ int main(void) {
   unsigned int frag = createShaderFromFile(GL_FRAGMENT_SHADER, "main.frag");
   unsigned int program = createShaderProgram(2, vert, frag);
 
+  float trailVertices[TRAIL_SIZE];
 
-  float vertices[] = {
-     1.0f,  1.0f,  0.0f,
-     1.0f, -1.0f,  0.0f,
-    -1.0f, -1.0f,  0.0f,
-    -1.0f,  1.0f,  0.0f,
-  }; 
-
-  int indices[] = {
-    0, 1, 3,
-    1, 2, 3
-  };
-
-  unsigned int VBO, VAO, EBO;
+  unsigned int VBO, VAO;
   glGenBuffers(1, &VBO);
   glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(trailVertices), trailVertices, GL_DYNAMIC_DRAW);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
-  glBindVertexArray(0);
+  double lastX = 0, lastY = 0;
 
-  if (glfwRawMouseMotionSupported())
-    glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
-
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  int valid = 0;
-  float positions[30][2];
-  
   while (!glfwWindowShouldClose(window)) {
-    double x, y;   
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    double x, y;
     glfwGetCursorPos(window, &x, &y);
 
     int w, h;
     glfwGetWindowSize(window, &w, &h);
 
-    if (valid < 30) {
-      valid++;
-    }
-    for (int i = 1; i < valid; i++) {
-      positions[i - 1][0] = positions[i][0];
-      positions[i - 1][1] = positions[i][1];
-    }
-    positions[valid - 1][0] = x;
-    positions[valid - 1][1] = h - y;
+    float ndcX = (2.0f * x / w) - 1.0f;
+    float ndcY = 1.0f - (2.0f * y / h);
+
+    float vectorX = x - lastX;
+    float vectorY = y - lastY;
+
+    float length = sqrt(pow(vectorY, 2) + pow(-vectorX, 2));
+     
+    float resultX = vectorY / length;
+    float resultY = -vectorX / length;
+
+    const float trailWidth = 25.0f;
+
+    trailVertices[0] = (2.0f * (x + (resultX * trailWidth)) / w) - 1.0f;
+    trailVertices[1] = 1.0f - (2.0f * (y + (resultY * trailWidth)) / h);
+    trailVertices[2] = (2.0f * (x - (resultX * trailWidth)) / w) - 1.0f;
+    trailVertices[3] = 1.0f - (2.0f * (y - (resultY * trailWidth)) / h);
 
     glUseProgram(program);
-    glUniform2fv(glGetUniformLocation(program, "positions"), 30, &positions[0][0]);
-    glUniform1i(glGetUniformLocation(program, "valid"), valid);
 
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(trailVertices), trailVertices);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, TRAIL_SIZE / 2);
+
+    for (int i = TRAIL_SIZE - 1; i >= 0 + 4; i -= 4) {
+      trailVertices[i - 3] = trailVertices[i - 7];
+      trailVertices[i - 2] = trailVertices[i - 6];
+      trailVertices[i - 1] = trailVertices[i - 5];
+      trailVertices[i] = trailVertices[i - 4];
+    }
+
+    lastX = x;
+    lastY = y;
 
     glfwSwapBuffers(window);
     glfwPollEvents();
